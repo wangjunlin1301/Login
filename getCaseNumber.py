58b8eed1c6e6a522128b5cb2c701097a1d7364f1
wang.junlin <wang.junlin@blackline.com>
2020-06-05 19:12:43 +0800

    update cases number

--- getCaseNumber.py
+++ getCaseNumber.py
@@ -2,29 +2,60 @@ import xlrd
 import xlwt
 
 
-def getTotalNumberEachModule():
+def getTotalNumberEachModule(Module):
 
-    Moudle = 'Accounts'
-    status = 'Passed'
+    # 获取文件数据
     Fliename = r'D:\Users\wang.junlin\桌面\testcases.xlsx'
     data = xlrd.open_workbook(Fliename, 'rb')
     sheets = data.sheet_by_index(0)
-    columns = sheets.col_slice(2, start_rowx=1)
+
+    # 模块
+    Moudlecol = sheets.col_slice(0, start_rowx=1)
+    Rowindex = 1
+    StatusCol = sheets.col_slice(2, start_rowx=Rowindex)
+
+    # 定义状态码的案例数量
     PassedNumber = 0
     KnownIssueNumber = 0
-    for i in columns:
-        i = str(i)
-        if "Passed" in i:
-            PassedNumber += 1
-        elif "Knownissue" in i:
-            KnownIssueNumber += 1
-        # print(type(str(i)))
+    FailedNumber = 0
+    BlockedNumber = 0
+    UntestNumber = 0
 
-    # print(PassedNumber, KnownIssueNumber)
-    # print(type(columns))
-    return PassedNumber
+    # 筛选模块
+    for m in Moudlecol:
+        Rowindex += 1
+        m = str(m)
+        if Module in m:
+            i = str(StatusCol[Rowindex - 2])
+            # 对字段条件进行判断
+            if "Passed" in i:
+                PassedNumber += 1
+            elif "Known" in i:
+                KnownIssueNumber += 1
+            elif "Failed" in i:
+                FailedNumber += 1
+            elif "Blocked" in i:
+                BlockedNumber += 1
+            elif "Untested" in i:
+                UntestNumber += 1
 
+    # 添加值进入字典
+    Status = {
+        "Module":
+        Module,
+        "Passed":
+        PassedNumber,
+        "Failed":
+        FailedNumber,
+        "KnownIssue":
+        KnownIssueNumber,
+        "Blocked":
+        BlockedNumber,
+        "Untest":
+        UntestNumber,
+        "Total":
+        PassedNumber + FailedNumber + KnownIssueNumber + BlockedNumber +
+        UntestNumber
+    }
 
-t = getTotalNumberEachModule()
-print(t)
-print(type(t))
+    return Status
--- pushToGoogle.py
+++ pushToGoogle.py
@@ -2,21 +2,44 @@
 
 import pygsheets
 import pandas as pd
+from getCaseNumber import getTotalNumberEachModule
 
+# Google Api认证
 googleauth = pygsheets.authorize(
     service_file='./khalil-test-278608-faf5f9854726.json')
 
-df = pd.DataFrame()
-sheetName = ['Accounts', 'CIM', 'Journals', 'Match']
-
-df['Name'] = ['khalil', 'tesasdasdast']
+sheetName = ['Accounts', 'CIM', 'Journals', 'Match', 'ICH', 'Task', 'Variance']
 
 #open the google spreadsheet ('pysheeetsTest' exists)
 sh = googleauth.open('Test for Regression')
 
-#select the first sheet
-for Name in sheetName:
-    wks = sh.worksheet_by_title(Name)
 
-    #update the first sheet with df, starting at cell B2
-    wks.set_dataframe(df, (1, 3))
+# 遍历上传
+def Upload():
+    for SN in sheetName:
+        StatusDict = getTotalNumberEachModule(SN)
+        Push(StatusDict)
+
+
+#select the first sheet
+def Push(StatusDict):
+
+    # 打开需要上传数据的相应模块工作表
+    Workspace = sh.worksheet_by_title(StatusDict['Module'])
+
+    # case数量赋值
+    Passed = StatusDict['Passed']
+    Failed = StatusDict['Failed']
+    KnownIssue = StatusDict['KnownIssue']
+    Blocked = StatusDict['Blocked']
+    Untested = StatusDict['Untest']
+    Total = StatusDict['Total']
+    NumberList = [Passed, Failed, KnownIssue, Blocked, Untested, Total]
+
+    # 创建上传的dataframe
+    df = pd.DataFrame(columns=NumberList)
+    Workspace.set_dataframe(df, (2, 6))
+
+
+if __name__ == "__main__":
+    Upload()
